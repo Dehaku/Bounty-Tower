2ba7d800e5cacd1e4b11e2e6660ad37b025c0fe9
Mika Attila <radiantstatue@gmail.com>
2015-01-27 13:33:58 +0100

    Remove some unused files

--- src/Weaponry.cpp
+++
@@ -1,7 +0,0 @@
-
-
-    //if(LMB == true){std::cout << "Ka-"; std::cout << "Boom" << endl;}
-
-
-
-
--- src/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-//#include <SFML/Window.hpp>
-
-
-
-
-#endif // MAIN_H_INCLUDED
