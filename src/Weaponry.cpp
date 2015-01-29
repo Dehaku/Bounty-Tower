749573ea0600ff4677957a9d6e703728d7a57de8
Mika Attila <radiantstatue@gmail.com>
2015-01-29 12:21:19 +0100

    Move Weaponry.h definitions to Weaponry.cpp

---
+++ src/Weaponry.cpp
@@ -0,0 +1,15 @@
+#include "Weaponry.h"
+#include "Globals.h"
+
+#include <iostream>
+
+void Explosion(int xpos, int ypos, int size, int power, bool frag)
+{
+    std::cout << CloseishS(xpos, ypos) << " Is the stuff\n";
+    //  std::cout << plat << std::endl;
+
+    //    std::vector<NPC>::iterator Targets;
+    //    for( Targets = npclist.begin(); Targets != npclist.end(); ++Targets ){
+
+    //    std::cout << Targets.name << std::endl;
+}
--- src/Weaponry.h
+++ src/Weaponry.h
@@ -1,20 +1,6 @@
 #ifndef __WEAPONRY_H__
 #define __WEAPONRY_H__
 
-// *#include <iostream>
-
-int CloseishS(int orix, int tarx);
-
-//#include "keys.h"
-extern bool Explosion(int xpos, int ypos, int size, int power, bool frag)
-{
-    std::cout << CloseishS(xpos, ypos) << " Is the stuff\n";
-    //  std::cout << plat << std::endl;
-
-    //    std::vector<NPC>::iterator Targets;
-    //    for( Targets = npclist.begin(); Targets != npclist.end(); ++Targets ){
-
-    //    std::cout << Targets.name << std::endl;
-}
+extern void Explosion(int xpos, int ypos, int size, int power, bool frag);
 
 #endif // WEAPONRY_H_INCLUDED
