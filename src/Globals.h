f42f73604a62c675e73ab87584386cbeea4332d8
Mika Attila <radiantstatue@gmail.com>
2015-01-30 14:05:11 +0100

    Move constant definitions from Globals.h to defs.h

--- src/Globals.h
+++ src/Globals.h
@@ -3,13 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 
-const sf::Vector2f Rez(1024, 768);
-const sf::Vector2f Center(500, 500);
-const int gridx = 100;
-const int gridy = 100;
-const int gridz = 10;
-const int GridSize = 20;
-const sf::Color Brown(100, 100, 0, 255);
+#include "defs.h"
 
 extern bool Debug;
 extern bool InFocus;
---
+++ src/defs.h
@@ -0,0 +1,14 @@
+#ifndef __DEFS_H_INCLUDED__
+#define __DEFS_H_INCLUDED__
+
+#include <SFML/Graphics.hpp>
+
+const sf::Vector2f Rez(1024, 768);
+const sf::Vector2f Center(500, 500);
+const int gridx = 100;
+const int gridy = 100;
+const int gridz = 10;
+const int GridSize = 20;
+const sf::Color Brown(100, 100, 0, 255);
+
+#endif // __DEFS_H_INCLUDED__
