66aea6b509ff7bf4a0f79b10283bbb1140ef6610
Mika Attila <radiantstatue@gmail.com>
2015-02-01 08:54:37 +0100

    Add autoformat script

---
+++ autoformat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from os import walk
+from subprocess import call
+
+ignore = [('src', 'main.cpp')]
+
+for (dirpath, dirnames, filenames) in walk('src'):
+    for f in filenames:
+        if (dirpath, f) not in ignore:
+            call(["clang-format", "-i", "{}/{}".format(dirpath, f)])
+
+print("Done.")
