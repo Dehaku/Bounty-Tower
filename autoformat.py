82938a6f43353f8e2619feee892d10ebdf38184b
Mika Attila <radiantstatue@gmail.com>
2015-02-02 14:46:51 +0100

    autoformat.py: Add noignore option

--- autoformat.py
+++ autoformat.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+import sys
 from os import walk
 from subprocess import call
+
+no_ignore = len(sys.argv) > 1 and sys.argv[1] == 'noignore'
+
+if no_ignore:
+        print("Warning: Formatting all files, including ignored ones")
 
 ignore = [
     ('src', 'main.cpp')  # Critter brain rewrite
@@ -9,7 +15,9 @@ ignore = [
 
 for (dirpath, dirnames, filenames) in walk('src'):
     for f in filenames:
-        if (dirpath, f) not in ignore:
+        if no_ignore or (dirpath, f) not in ignore:
             call(["clang-format", "-i", "{}/{}".format(dirpath, f)])
+        else:
+            print("Ignoring {}/{}".format(dirpath, f))
 
 print("Done.")
