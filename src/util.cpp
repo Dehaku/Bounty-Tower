f20c21250f77d9d311539ae79dc495add09d1bf2
Dehaku <dehakuzedon@gmail.com>
2015-10-20 12:34:55 -0500

    Possibly improved random()'s randomness.

--- src/util.cpp
+++ src/util.cpp
@@ -54,11 +54,16 @@ int randz(int minValue, int maxValue)
     return (minValue + rawRand);
 }
 
+unsigned lastSeed = 0;
+
 int random(int minValue, int maxValue)
 {
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    seed += randz(1,100); // Doing this because random() doesn't seem to reset quick enough for rapid random numbers.
-    std::minstd_rand0 generator (seed); // minstd_rand0 is a standard linear_congruential_engine
+
+    if(lastSeed == 0)
+        lastSeed = std::chrono::system_clock::now().time_since_epoch().count();
+    else
+        lastSeed += randz(1,100); // Doing this because random() doesn't seem to reset quick enough for rapid random numbers.
+    std::minstd_rand0 generator (lastSeed); // minstd_rand0 is a standard linear_congruential_engine
 
     maxValue = maxValue + 1;
     if (minValue == maxValue)
