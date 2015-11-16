032ad58031d6b14d016543c28873736277bb6aa6
Dehaku <dehakuzedon@gmail.com>
2015-11-16 15:30:09 -0600

    Made Skills load.

--- src/SaveLoad.cpp
+++ src/SaveLoad.cpp
@@ -126,102 +126,7 @@ void saveGame(std::string profileName)
                 }
             }
         }
-
-
-
-
-
-
-
-
-        /*
-        std::ofstream outputFileItems(invLine.c_str());
-
-        for (auto i = critter.inventory.begin(); i != critter.inventory.end();
-             i++)
-        {
-            outputFile << "[" << (*i).name << "]";
-        }
-        outputFile << "";
-
-        */
-
     }
-
-
-
-
-
-    /*
-
-    std::ofstream file;
-    con("looking for file...");
-    file.open(line.c_str(), std::fstream::in | std::fstream::ate);
-    if (file.is_open())
-    {
-        con("Problem? 3");
-        file << "[name:" << critter.name << "]"
-             << "[race:" << critter.race << "]"
-             << "[xpos:" << critter.xpos << "]"
-             << "[ypos:" << critter.ypos << "]"
-             << "[strength:" << critter.attributes.strength << "]"
-             << "[perception:" << critter.attributes.perception << "]"
-             << "[intelligence:" << critter.attributes.intelligence << "]"
-             << "[charisma:" << critter.attributes.charisma << "]"
-             << "[endurance:" << critter.attributes.endurance << "]"
-             << "[dexterity:" << critter.attributes.dexterity << "]"
-             << "[agility:" << critter.attributes.agility << "]"
-             << "[health:" << critter.health << "]"
-             //<< "[action:" << critter.action << "]"
-             << "[angle:" << critter.angle << "]"
-             << "[thirst:" << critter.thirst << "]"
-             << "[hunger:" << critter.hunger << "]"
-             << "[strengthxp:" << critter.attributes.strengthxp << "]"
-             << "[perceptionxp:" << critter.attributes.perceptionxp << "]"
-             << "[intelligencexp:" << critter.attributes.intelligencexp << "]"
-             << "[charismaxp:" << critter.attributes.charismaxp << "]"
-             << "[endurancexp:" << critter.attributes.endurancexp << "]"
-             << "[dexterityxp:" << critter.attributes.dexterityxp << "]"
-             << "[agilityxp:" << critter.attributes.agilityxp << "]"
-             << "[cbaseid:" << critter.cbaseid << "]"
-             << "[maxhealth:" << critter.maxhealth << "]"
-             << "{Tags:" << critter.tags << "}"
-             << "{Blood:" << critter.bloodcontent << "}"
-             << "{Items:";
-        con("Problem? 4");
-
-        //for(int i = 0; i != Critter.inventory.size(); i++)
-        for (auto i = critter.inventory.begin(); i != critter.inventory.begin();
-             i++)
-        {
-            file << "[" << (*i).name << "]";
-        }
-        file << "}";
-        file << std::endl;
-        con("Problem? 5");
-        //Con("Added", false);
-        //Con(object.name);
-        file.close();
-    } // End to Load Map
-
-    */
-    /*debug("v-Adding Tags-v");
-                debug(StringFindChaos(line,"{Tags:","}"));
-                debug("^-Added Tags-^");
-                Critter.tags.append(StringFindChaos(line,"{Tags:","}"));
-                std::set<std::string> Items;
-                std::string List;
-                List.append(StringFindChaos(line,"{Items:","}"));
-                Items = StringFindSetChaos(List,"[","]");
-                for(std::set<std::string>::iterator i = Items.begin(); i != Items.end(); i++)
-                {
-                    std::string OogaBooga = *i;
-                    char *a=new char[OogaBooga.size()+1];a[OogaBooga.size()]=0;memcpy(a,OogaBooga.c_str(),OogaBooga.size());
-                    if(Debug){ std::cout << "Pre Critter.AddItem \n";}
-                    Critter.AddItem(a,1);
-                    if(Debug){ std::cout << "*Post Critter.AddItem* \n";}
-                }*/
-
 }
 
 void loadGame(std::string profileName)
@@ -272,52 +177,15 @@ void loadGame(std::string profileName)
         critter.health = critter.getMaxHealth();
         //status.auraAffectsAllies = booleanize(stringFindNumber(line, "[AuraAllies:"));
 
-
-
-        /*
-        std::vector<std::string> aspects = stringFindVectorChaos(line,"[Aspect:","]");
-
-        for(auto &aspect : aspects)
+        con("Loading Skills");
+        std::vector<std::string> skills = stringFindVectorChaos(line,"[Skill:","]");
+        for(auto &skillLine : skills)
         {
-            std::cout << "Found: " << aspect << std::endl;
-            std::vector<std::string> components = stringFindElements(aspect,":");
-            for(int i = 0; i != components.size(); i++)
-            {
-                if(i == 0)
-                    std::cout << "Name: " << components[i] << std::endl;
-                if(i == 1)
-                    std::cout << "Potency: " << components[i] << std::endl;
-                if(i == 2)
-                    std::cout << "Type: " << components[i] << std::endl;
-            }
-
-            StatusAspect SA;
-            for(int i = 0; i != components.size(); i++)
-            {
-                if(i == 0)
-                {
-                    for(int t = 0; t != aspectNum.size(); t++)
-                    {
-                        if(aspectNum[t] != components[i])
-                            continue;
-                        SA.name = t;
-                        break;
-                    }
-                }
-                if(i == 1)
-                    SA.potency = std::stoi(components[i]);
-                if(i == 2)
-                    SA.type = components[i];
-            }
-
-            status.aspects.push_back(SA);
-            std::cout << "New Aspect: " << aspectNum[SA.name] << ": " << SA.name << ", Pot: " << SA.potency << ", Type: " << SA.type << std::endl;
-
+            std::vector<std::string> skillBits = stringFindElements(skillLine);
+            for(auto &skill : critter.skills.list)
+                if(skillBits[0] == skill.name)
+                    skill.ranks = std::stoi(skillBits[1]);
         }
-
-        std::cout << "Adding Status Effect: " << status.name << std::endl;
-        statusEffects.push_back(status);
-        */
 
         critter.isSquaddie = true;
         critter.faction = conFact->name;
@@ -325,8 +193,6 @@ void loadGame(std::string profileName)
         critter.chasePriority = "Hold Position";
         std::cout << "Loaded " << critter.name << " the " << critter.race << std::endl;
         npclist.push_back(critter);
-        //setupSquadHotKeySelection();
-
     }
 
     AnyDeletes(npclist);
